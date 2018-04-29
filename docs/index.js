ef79cb57f0e72487e4cdd3ae06f902e40f57bf2c
Yaodong Li <ydli@thoughtworks.com>
2018-04-29 22:22:37 +0800

    Add rotate control

--- docs/index.js
+++ docs/index.js
@@ -1,22 +1,72 @@
+// draw a line betwen two position
+const lineBetween = function(B, A) {
+    let coords = [];
+
+    const N = 100;
+
+    const m = (A.lt - A.lg) / (B.lt - B.lg);
+    for (let i = 0; i < N; ++i) {
+        const lt = A.lt - i / N * (A.lt - B.lt);
+        const lg = A.lg - i / N * (A.lg - B.lg);
+
+        coords.push([lt, lg]);
+        coords.unshift([lt, lg]);
+    }
+
+    return coords;
+};
+
+// Start a simple rotation animation
+const rotateEarth = earth => {
+    let before = null;
+    let needAnim = true;
+
+    requestAnimationFrame(function animate(now = 0) {
+        if (!needAnim) {
+            return;
+        }
+
+        const c = earth.getPosition();
+        const elapsed = before ? now - before : 0;
+        before = now;
+        earth.setCenter([c[0], c[1] + 0.1 * (elapsed / 30)]);
+        requestAnimationFrame(animate);
+    });
+
+    return () => {
+        needAnim = false;
+    };
+};
+
 function initialize() {
-    const options = {atmosphere: true, center: [0, 0], zoom: 3};
+    const options = {
+        atmosphere: true,
+        center: [0, 0],
+        sky: true,
+        zoom: 3,
+    };
+
     const earth = new WE.map('earth_div', options);
 
     WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
         minZoom: 0,
         maxZoom: 5,
-        attribution: 'NASA'
+        attribution: 'NASA',
     }).addTo(earth);
 
-    // Start a simple rotation animation
-    let before = null;
+    const A = { lt: 40.7306, lg: -73.9352 };
+    const B = { lt: 47.4979, lg: 19.0402 };
 
-    requestAnimationFrame(function animate(now = 0) {
-        const c = earth.getPosition();
-        const elapsed = before ? now - before: 0;
-        before = now;
-        earth.setCenter([c[0], c[1] + 0.1 * (elapsed/30)]);
-        requestAnimationFrame(animate);
+    WE.polygon(lineBetween(B, A)).addTo(earth);
+
+    let cancelRotate = rotateEarth(earth);
+
+    $('#option-rotate').on('change', e => {
+        if (e.currentTarget.checked) {
+            cancelRotate = rotateEarth(earth);
+        } else {
+            cancelRotate();
+        }
     });
 }
 
